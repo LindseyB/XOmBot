fcf3f9fea94214cb9259419c3fea26979c60c698
LindseyB <hazardous.narayan@gmail.com>
2009-07-03 16:29:01 -0400

    It really helps if I set the bot name back to normal and have it join the right channel. README has been updated.

--- XOmBot.pl
+++ XOmBot.pl
@@ -18,12 +18,12 @@ my $commitid = "";
 my $conn = $irc->newconn(
 	Server 		=> shift || 'irc.freenode.net',      # the network to connect to
 	Port		=> shift || '6667',                  # the port to use for the connection
-	Nick		=> 'XOmButt',
+	Nick		=> 'XOmBot',
 	Ircname		=> 'Resident XOmbie',
 	Username	=> 'bot'
 );
 
-$conn->{channel} = shift || '#testchan';                  # the channel to join on successful connect
+$conn->{channel} = shift || '#xomb';                  # the channel to join on successful connect
 
 
 
