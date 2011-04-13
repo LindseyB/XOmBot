c662b83f0bb3111d069e33e75ded22039fef4e74
wolfwood <wolfwood@xomb.org>
2011-04-13 10:27:33 +0800

    feeling lucky

--- XOmBot.pl
+++ XOmBot.pl
@@ -64,7 +64,7 @@ sub said {
 				# show all the commands that xombot listens to
 				$self->say(channel => $channel, body => "!wiki [search term] - will search the wiki for the given word or phrase.");
 				$self->say(channel => $channel, body => "!latest - will show the last commit to the offical XOmB repository.");
-				$self->say(channel => $channel, body => "!google [phrase] for [nick] - answer questions.");
+				$self->say(channel => $channel, body => "!google [phrase] for [nick] - answer questions. More bangs to shoot from the hip.");
 				$self->say(channel => $channel, body => "!coinflip - ...");
 				$self->say(channel => $channel, body => "!santa - ask Santa whether $mynick has been naughty or nice.");
 		}
@@ -79,13 +79,18 @@ sub said {
 				$commitid = "";
 		}
 
-		if($body =~ m/^\!google (.*) for (.*)/){
-				my($term) = $1;
-				my($target) = $2;
-						
+		if($body =~ m/^\!(!?)google(!?) (.*) for (.*)/){
+				my($term) = $3;
+				my($target) = $4;
+				my($lucky);
+
+				if($1 ne "" || $2 ne ""){
+						$lucky = "&l=1";
+				}
+
 				$term =~ s/ /+/g;
 						
-				$self->say(channel => $channel, who => $target, address => "1", body => "http://lmgtfy.com/?q=$term");
+				$self->say(channel => $channel, who => $target, address => "1", body => "http://lmgtfy.com/?q=$term$lucky");
 		}
 
 		if($body =~ m/^\!coinflip/){
