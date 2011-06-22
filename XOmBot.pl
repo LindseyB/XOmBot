60316d43ada1ce72ffd6c403eb70de692f0d4549
LindseyB <hazardous.narayan@gmail.com>
2011-06-22 02:03:35 -0400

    formatting

--- XOmBot.pl
+++ XOmBot.pl
@@ -4,13 +4,13 @@
 # XomBot.pl [nick]                                                              #
 #                                                                               #
 # Your friendly XOmB bot for #xomb												#
-#																				#
-# This program is free software. It comes without any warranty, to the extent	#
-# permitted by applicable law. You can redistribute it and/or modify it under 	#
-# the term of the Do What The Fuck You Want To Public License, Version 2, as	#
-# published by Sam Hocevar. See http://sam.zoy.org/wtfpl/COPYING for more 		#
-# details 																		#
-# 																				#
+#                                                                               #
+# This program is free software. It comes without any warranty, to the extent   #
+# permitted by applicable law. You can redistribute it and/or modify it under   #
+# the term of the Do What The Fuck You Want To Public License, Version 2, as    #
+# published by Sam Hocevar. See http://sam.zoy.org/wtfpl/COPYING for more       #
+# details                                                                       #
+#                                                                               #
 #################################################################################
 
 package Bot;
