f9f67ce9124641c7c15ab6cef8b1fd623b0ea56d
wilkie <wilkie05@gmail.com>
2012-02-04 23:58:39 -0500

    Patching Cinch to write out action messages from Message.

--- lib/xombot.rb
+++ lib/xombot.rb
@@ -1,6 +1,15 @@
 require 'bundler'
 Bundler.require
 
+# allow sending of actions
+module Cinch
+  class Message
+    def emote(m)
+      reply "\001ACTION #{m}\001"
+    end
+  end
+end
+
 require 'xombot/plugin'
 
 # Place all plugins into a module
@@ -9,6 +18,8 @@ Dir[File.dirname(__FILE__) + '/xombot/plugins/*.rb'].each do |file|
 end
 
 module XOmBot
+  NAME = "XOmBot-test"
+
   class << self
     attr_reader :plugins
 
@@ -20,13 +31,17 @@ module XOmBot
     def load_plugins
     end
 
+    def name
+      NAME
+    end
+
     def start
       bot = Cinch::Bot.new do
         configure do |c|
           c.server = "irc.freenode.org"
           c.port = 6697
           c.ssl.use = true
-          c.nick = "XOmBot-test"
+          c.nick = NAME 
           c.channels = ["#XOmBot"]
           c.plugins.plugins = XOmBot::Plugins.constants.map do |plugin|
             XOmBot::Plugins.const_get(plugin)
