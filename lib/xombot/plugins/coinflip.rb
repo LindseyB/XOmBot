7a0ac88913524354c15cb973582fb0b0378a621d
wilkie <wilkie05@gmail.com>
2012-02-05 00:24:03 -0500

    Refactor regex for !coinflip slightly.

--- lib/xombot/plugins/coinflip.rb
+++ lib/xombot/plugins/coinflip.rb
@@ -1,10 +1,10 @@
 class Coinflip < XOmBot::Plugin
-  match /coinflip( (.+) or (.+))?/
+  match /coinflip(?: (.+) or (.+))?/
   help "Flips a coin"
   usage "coinflip -- Returns either heads or tails"
   usage "coinflip x or y -- Returns either x or y"
 
-  def execute(m, whole = nil, heads = nil, tails = nil)
+  def execute(m, heads = nil, tails = nil)
     result = rand(2)
     if result == 0
       if heads
