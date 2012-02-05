856302fc467d59198f6245ab6962d949e1cdac59
wilkie <wilkie05@gmail.com>
2012-02-04 23:41:02 -0500

    Added help command.

---
+++ lib/xombot/plugins/help.rb
@@ -0,0 +1,19 @@
+class Help < XOmBot::Plugin
+  match /help (.+)/
+  help "Displays information on command arguments"
+
+  def execute(m, command)
+    XOmBot.plugins.each do |p|
+      idx = p.commands.keys.map{|c| c.inspect.to_s[1..-2][/\w+/]}.index command
+      if not idx.nil?
+        info = p.commands[p.commands.keys[idx]]
+        m.reply "[#{command}] #{info[:help]}"
+        if info[:usage]
+          info[:usage].each do |usage|
+            m.reply usage
+          end
+        end
+      end
+    end
+  end
+end
