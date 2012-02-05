36932b1764c85b40fdea6c3258b8ea4c58e96e5f
wilkie <wilkie05@gmail.com>
2012-02-04 23:40:33 -0500

    No punctuation in help strings.

--- lib/xombot/plugins/hello.rb
+++ lib/xombot/plugins/hello.rb
@@ -2,7 +2,7 @@ require 'xombot/plugin'
 
 class Hello < XOmBot::Plugin
   match "hello"
-  help "This command says hello."
+  help "This command says hello"
 
   def execute(m)
     m.reply "Hello, #{m.user.nick}"
