cbc36c59adceeb6e64a6a31ec0521547e4bcbf07
wilkie <wilkie05@gmail.com>
2012-02-22 21:18:33 -0500

    Adds the LocalTime plugin that queries the local time of a given city.

---
+++ lib/xombot/plugins/local_time.rb
@@ -0,0 +1,15 @@
+class LocalTime < XOmBot::Plugin
+  TIME_QUERY_URL = "http://www.timeanddate.com/worldclock/results.html?query="
+
+  match /time (.+)/
+  help "gives the local time in the given city"
+  usage "time Melbourne -- gives the time in Melbourne Australia"
+
+  def execute(m, place)
+    @agent = Mechanize.new
+    page = @agent.get "#{TIME_QUERY_URL}#{place}"
+    current_time = page.search '//strong[@id="ct"]'
+
+    m.reply "Time in #{place}: #{current_time.first.content}"
+  end
+end
