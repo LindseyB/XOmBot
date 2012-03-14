b4851f26ab1e9016c0bd3412a72cd5c707239f3b
LindseyB <hazardous.narayan@gmail.com>
2012-03-14 16:36:23 -0400

    Added a usage to tweet to display the latest tweet by a given username.

--- lib/xombot/plugins/tweet.rb
+++ lib/xombot/plugins/tweet.rb
@@ -1,10 +1,18 @@
 class Tweet < XOmBot::Plugin
-  match /tweet (\d+)/
+  match /tweet (\d+)/, :method => :tweet_by_id
   help "Displays the tweet with the given id"
   usage "tweet 12345678 -- displays the twitter update with that id"
+  
+  match /tweet (\w.*)/, :method => :tweet_by_username
+  help "Displays the latest tweet by the given user"
+  usage "tweet noob -- displays the last tweet by noob"
 
-  def execute(m, id)
+  def tweet_by_id(m, id)
     status = Twitter.status(id)
     m.reply "@#{status.user.screen_name}: #{status.text}"
+  end
+
+  def tweet_by_username(m, username)
+   m.reply "@#{username}: #{Twitter.user_timeline(username).first.text}" 
   end
 end
