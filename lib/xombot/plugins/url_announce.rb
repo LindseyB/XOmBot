3a894f55841bcbe4e86f61a191634cbdf1efaba3
wilkie <wilkie05@gmail.com>
2012-10-14 17:59:39 -0400

    Adds ability to override url parsing in URLAnnounce.

--- lib/xombot/plugins/url_announce.rb
+++ lib/xombot/plugins/url_announce.rb
@@ -6,10 +6,34 @@ class URLAnnounce < XOmBot::Plugin
     return if ignore_plugin and ignore_plugin.ignored.include? m.user.nick
 
     m.message.scan /https?:\/\/[\S]+/ do |url|
+      dispatch m, url
+    end
+  end
+
+  def dispatch(m, url)
+    domain = url[/https?:\/\/(?:[^.]+\.)?([^.]+\.[^\/]+)(?:\/|$)/, 1]
+
+    @callback = @callback || {}
+    if @callback[domain]
+      @callback[domain].call m, url
+    else
+      default_announce m, url
+    end
+  end
+
+  def match_domain(domain, &block)
+    @callback = @callback || {}
+    @callback[domain] = block
+  end
+
+  def default_announce(m, url)
+    begin
       page = Mechanize.new.get url
+
       if page.is_a? Mechanize::Page
         m.reply "Title: #{page.title.gsub(/\t|\r|\n/, " ").strip}"
       end
+    rescue
     end
   end
 end
