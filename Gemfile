f40d9d4dfb18b4e83311ea4683a018a0d63a447e
Lindsey Bieda <hazardous.narayan@gmail.com>
2013-03-16 00:08:05 -0400

    FUCK YOU TWITTER!!!!
    After the twitter gem and API have proven to be flawed and unreliable
    I've decided to remove gut it.
    
    I've resorted to screenscraping for tweets. I'm not proud of it,
    It works, however, and I am losing zero functionality. Though, responce
    times will be slower since instead of fetching a tiny JSON object the
    entire web page is being fetched and then parsed with nokogiri. I'm
    doing some magic with XPATH since, unfortunately, nokogiri did not
    like me having two nots in my css selectors.
    
    Note the relevant ticket: https://github.com/sparklemotion/nokogiri/issues/451

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source :rubygems
 gem "cinch", "~> 1.1.3"
 gem "mechanize"
-gem "twitter"
+gem "nokogiri"
 gem "htmlentities"
 gem "json"
--- lib/xombot/plugins/tweet.rb
+++ lib/xombot/plugins/tweet.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 class Tweet < XOmBot::Plugin
   match /tweet (\d+)/, :method => :tweet_by_id
   help "Displays the tweet with the given id"
@@ -17,16 +19,19 @@ class Tweet < XOmBot::Plugin
   end
 
   def tweet_by_id(m, id)
-    status = Twitter.status(id)
-    m.reply "@#{status.user.screen_name}: #{HTMLEntities.new.decode status.text}"
+    tweet_by_url m, "https://twitter.com/a/status/#{id}"
   end
 
   def tweet_by_username(m, username)
-    m.reply "@#{username}: #{HTMLEntities.new.decode Twitter.user_timeline(username).first.text}" 
+    doc = Nokogiri::HTML(open("https://twitter.com/#{username}"))
+    # terrible selector to grab the first tweet that isn't a reply or retweeted
+    tweet = doc.xpath("//*[contains(concat(' ', @class, ' '), ' original-tweet ') and not(@data-is-reply-to = \"true\") and not(@data-retweet-id)]")
+               .first
+    tweet ? tweet_by_id(m, tweet.attr('data-tweet-id')) : m.reply("#{username} doesn't have any recent tweets")
   end
 
   def tweet_by_url(m, url)
-    id = url[/^https?:\/\/.*\/status\/(\d+)/,1]
-    tweet_by_id m, id
+    doc = Nokogiri::HTML(open(url))
+    m.reply "#{doc.css('.permalink-tweet-container .username.js-action-profile-name').first.text}: #{doc.css('.tweet-text').first.text}"
   end
 end
