33c0a4391b91edba9935faa3d4e87f89bc9bea51
Lindsey Bieda <hazardous.narayan@gmail.com>
2014-12-13 22:58:11 -0500

    Weather works again, no more hourly, can be specific with locations

--- lib/xombot/plugins/weather.rb
+++ lib/xombot/plugins/weather.rb
@@ -1,50 +1,27 @@
 class Weather < XOmBot::Plugin
-  WEATHER_QUERY_URL = "http://weather.com/search/enhancedlocalsearch?where="
+  WEATHER_QUERY_URL = "http://www.wunderground.com/cgi-bin/findweather/hdfForecast?query="
 
   match /weather ([^ ]+)(?:\s(.+))?/
   help "gives the forecast for the given city"
   usage "weather pittsburgh -- gives the weather for pittsburgh"
-  usage "weather melbourne hourly -- gives a 6 hour forecase for melbourne"
+  usage "weather melbourne, australia -- gives the weather for melbourne australia"
 
-  def execute(m, place, option)
+  def execute(m, place, place2)
     @agent = Mechanize.new
-    page = @agent.get "#{WEATHER_QUERY_URL}#{place}"
-    current_weather = page.search '//span[@itemprop="temperature-fahrenheit"]'
-    current_phrase  = page.search '//div[@class="wx-phrase "]'
+    page = @agent.get "#{WEATHER_QUERY_URL}#{place}#{place2}"
 
-    if option == "hourly"
-      hourly_url = page.search '//a[@from="rightnow_1"]'
-      if hourly_url.first.nil?
-        hourly_url = page.search '//a[@from="today_TimeNav_weather_nav"][@title="Hourly"]'
-      end
-
-      if hourly_url.first.nil?
-        m.reply "No hourly forecast available."
-        return
-      end
-
-      hourly_url = hourly_url.first.attribute "href"
-      page = @agent.get "http://weather.com#{hourly_url}"
-
-      parts = page.search('//div[@class="wx-timepart"]').to_a
-      parts.insert(0, page.search('//div[@class="wx-timepart wx-first"]'))
-
-      reply_string = parts.inject("") do |a, part|
-        hour = part.children[1].children[0].text.to_i
-        meridian = part.children[1].children[1].text.strip
-        if meridian.upcase == "PM"
-          hour += 12
-        end
-        temp = part.children[3].children[2].text.strip.to_i
-        phrase = part.children[3].children[4].text
-        temp_cel = (temp - 32) * 5 / 9
-        "#{a} #{hour}:00: #{temp}\u00b0F/#{temp_cel}\u00b0C/#{phrase}"
-      end
-      m.reply "#{place} by hour:#{reply_string}"
+    # unfortunately their 404 page returns 200 :(
+    if page.at('body').text.include? "Error 404: Page Not Found"
+      m.reply "Unable to weather for #{place}, are you sure it exists?"
+    elsif page.at('body').attr('class').include? "not-set"
+      m.reply "Ambiguous location, can you be more specific?"
     else
-      temp = current_weather.first.text.to_i
+      current_weather = page.search 'span[data-variable="temperature"] .wx-value'
+      current_phrase  = page.search 'div[data-variable="condition"] .wx-value'
+
+      temp = current_weather.text.to_i
       temp_cel = (temp - 32) * 5 / 9
-      m.reply "Weather in #{place}: #{temp}\u00b0F/#{temp_cel}\u00b0C and #{current_phrase.first.text}"
+      m.reply "Weather in #{place}: #{temp}\u00b0F/#{temp_cel}\u00b0C and #{current_phrase.text}"
     end
   end
 end
